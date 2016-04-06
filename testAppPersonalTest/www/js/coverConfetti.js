3c1c044c591fc3600516f429960e0df179c798ee
rberman <rberman@macalester.edu>
2016-04-06 15:01:36 -0500

    Confetti updated to be centered and extraneous code removed.

--- testAppPersonalTest/www/js/coverConfetti.js
+++ testAppPersonalTest/www/js/coverConfetti.js
@@ -1,92 +1,49 @@
 //Adapted from a codepen on https://davidwalsh.name/ blog.
 
 var utils = {
-  norm: function(value, min, max) {
+  norm: function (value, min, max) {
     return (value - min) / (max - min);
   },
 
-  lerp: function(norm, min, max) {
+  lerp: function (norm, min, max) {
     return (max - min) * norm + min;
   },
 
-  map: function(value, sourceMin, sourceMax, destMin, destMax) {
+  map: function (value, sourceMin, sourceMax, destMin, destMax) {
     return utils.lerp(utils.norm(value, sourceMin, sourceMax), destMin, destMax);
   },
 
-  clamp: function(value, min, max) {
-    return Math.min(Math.max(value, Math.min(min, max)), Math.max(min, max));
-  },
-
-  distance: function(p0, p1) {
+  distance: function (p0, p1) {
     var dx = p1.x - p0.x,
       dy = p1.y - p0.y;
     return Math.sqrt(dx * dx + dy * dy);
   },
 
-  distanceXY: function(x0, y0, x1, y1) {
-    var dx = x1 - x0,
-      dy = y1 - y0;
-    return Math.sqrt(dx * dx + dy * dy);
-  },
-
-  circleCollision: function(c0, c1) {
-    return utils.distance(c0, c1) <= c0.radius + c1.radius;
-  },
-
-  circlePointCollision: function(x, y, circle) {
-    return utils.distanceXY(x, y, circle.x, circle.y) < circle.radius;
-  },
-
-  pointInRect: function(x, y, rect) {
-    return utils.inRange(x, rect.x, rect.x + rect.width) &&
-      utils.inRange(y, rect.y, rect.y + rect.height);
-  },
-
-  inRange: function(value, min, max) {
-    return value >= Math.min(min, max) && value <= Math.max(min, max);
-  },
-
-  rangeIntersect: function(min0, max0, min1, max1) {
-    return Math.max(min0, max0) >= Math.min(min1, max1) &&
-      Math.min(min0, max0) <= Math.max(min1, max1);
-  },
-
-  rectIntersect: function(r0, r1) {
-    return utils.rangeIntersect(r0.x, r0.x + r0.width, r1.x, r1.x + r1.width) &&
-      utils.rangeIntersect(r0.y, r0.y + r0.height, r1.y, r1.y + r1.height);
-  },
-
-  degreesToRads: function(degrees) {
+  degreesToRads: function (degrees) {
     return degrees / 180 * Math.PI;
   },
 
-  radsToDegrees: function(radians) {
-    return radians * 180 / Math.PI;
-  },
-
-  randomRange: function(min, max) {
+  randomRange: function (min, max) {
     return min + Math.random() * (max - min);
   },
 
-  randomInt: function(min, max) {
+  randomInt: function (min, max) {
     return Math.floor(min + Math.random() * (max - min + 1));
   }
 
-}
+};
 
-var coverCanvas, coverContext, W, H, generator1
-gravity = parseFloat(0.1)
-wind = 0
-friction = 0.99;
+var coverCanvas, coverContext, W, H, generator1,
+  gravity = parseFloat(0.1),
+  wind = 0,
+  friction = 0.99;
 
-function coverCanvasInit(){
+function coverCanvasInit() {
   coverCanvas = document.getElementById("coverCanvas");
-  W = coverCanvas.width = window.innerWidth;
+  W = coverCanvas.width = window.innerWidth * 0.8;
   H = coverCanvas.height = window.innerHeight;
   coverContext = coverCanvas.getContext("2d");
 }
-
-//generatorStock = [];
 
 colors = [
   '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
@@ -100,7 +57,6 @@ colors = [
 //
 
 generator1 = new particleGenerator(0, 0, W, 0, 200);
-
 
 
 function randomInt(min, max) {
@@ -117,7 +73,7 @@ function particle(x, y) {
   this.y = y;
   this.vx = randomInt(-4, 4);
   this.vy = randomInt(-10, -0);
-  this.type = utils.randomInt(0,1);
+  this.type = utils.randomInt(0, 1);
 
   this.w = utils.randomRange(5, 20);
   this.h = utils.randomRange(5, 20);
@@ -131,7 +87,7 @@ function particle(x, y) {
   this.rotateY = randomInt(0, 1);
 }
 
-particle.prototype.update = function() {
+particle.prototype.update = function () {
   this.x += this.vx;
   this.y += this.vy;
   this.vy += gravity;
@@ -159,34 +115,34 @@ particle.prototype.update = function() {
   coverContext.beginPath();
   coverContext.fillStyle = this.color;
   coverContext.strokeStyle = this.color;
-  coverContext.lineCap= "round";
+  coverContext.lineCap = "round";
   coverContext.lineWidth = 2;
 
   if (this.type === 0) {
     coverContext.beginPath();
     coverContext.arc(0, 0, this.r, 0, 2 * Math.PI);
     coverContext.fill();
-  }else if(this.type === 2){
+  } else if (this.type === 2) {
     coverContext.beginPath();
     for (i = 0; i < 22; i++) {
       angle = 0.5 * i;
       x = (.2 + 1.5 * angle) * Math.cos(angle);
-      y =(.2 + 1.5 * angle) * Math.sin(angle);
+      y = (.2 + 1.5 * angle) * Math.sin(angle);
 
       coverContext.lineTo(x, y);
     }
     coverContext.stroke();
 
-  }else if(this.type === 1){
+  } else if (this.type === 1) {
     coverContext.fillRect(-this.w / 2, -this.h / 2, this.w, this.h);
   }
 
   coverContext.closePath();
   coverContext.restore();
-}
+};
 
 function particleGenerator(x, y, w, h, number, text) {
-  // particle will spawn in this aera
+  // particle will spawn in this area
   this.x = x;
   this.y = y;
   this.w = w;
@@ -196,10 +152,10 @@ function particleGenerator(x, y, w, h, number, text) {
   this.text = text;
   this.recycle = true;
 
-  this.type=1;
-
+  this.type = 1;
 }
-particleGenerator.prototype.animate = function() {
+
+particleGenerator.prototype.animate = function () {
 
   coverContext.fillStyle = "grey";
 
@@ -210,7 +166,6 @@ particleGenerator.prototype.animate = function() {
   coverContext.textAlign = "center";
 
   coverContext.closePath();
-  //	   for (var i = 0; i < 100; i++) {
 
   if (this.particles.length < this.number) {
     this.particles.push(new particle(clamp(randomInt(this.x, this.w + this.x), this.x, this.w + this.x),
@@ -225,40 +180,36 @@ particleGenerator.prototype.animate = function() {
   for (var i = 0; i < this.particles.length; i++) {
     var p = this.particles[i];
     p.update();
-    if (p.y > H || p.y < -100 || p.x > W+100 || p.x < -100&& this.recycle) {
+    if (p.y > H || p.y < -100 || p.x > W + 100 || p.x < -100 && this.recycle) {
       //a brand new particle replacing the dead one
       this.particles[i] = new particle(clamp(randomInt(this.x, this.w + this.x), this.x, this.w + this.x),
 
         clamp(randomInt(this.y, this.h + this.y), this.y, this.h + this.y), this.text);
     }
   }
-}
+};
 
 
-function toggleEngine(){
-  if(generator1.type ===0){
-    generator1.type =1;
-    generator1.x =W/2;
-    generator1.y =H/2;
-    generator1.w =0;
-
-  }else{
-    generator1.type =0;
-    generator1.x =1;
-    generator1.w =W;
-    generator1.y =0;
-
+function toggleEngine() {
+  if (generator1.type === 0) {
+    generator1.type = 1;
+    generator1.x = W / 2;
+    generator1.y = H / 2;
+    generator1.w = 0;
   }
-
+  else {
+    generator1.type = 0;
+    generator1.x = 1;
+    generator1.w = W;
+    generator1.y = 0;
+  }
 }
-
 
 function updateConfetti() {
-  // context.globalAlpha=.5;
-  coverCanvasInit()
-  toggleEngine()
+  coverCanvasInit();
+  toggleEngine();
 
-  coverContext.fillStyle="white";
+  coverContext.fillStyle = "white";
   coverContext.clearRect(0, 0, W, H);
   generator1.animate();
   requestAnimationFrame(updateConfetti);
